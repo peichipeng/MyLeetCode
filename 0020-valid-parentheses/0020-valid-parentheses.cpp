de33b7b714b54d36d1ad2f031a1073f64528fa0d
peichipeng <118533573+peichipeng@users.noreply.github.com>
2025-01-11 22:45:48 +0800

    Time: 0 ms (100%), Space: 9.8 MB (7.69%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                st.push(')');
+            } else if (s[i] == '{') {
+                st.push('}');
+            } else if (s[i] == '[') {
+                st.push(']');
+            } else {
+                if (st.empty() || s[i] != st.top()) return false;
+                st.pop();
+            }
+        }
+        if (!st.empty()) return false;
+        return true;
+    }
+};
