685564f051664858ef3918f19024d80aa563787e
peichipeng <118533573+peichipeng@users.noreply.github.com>
2025-01-18 14:06:23 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        sort(potions.begin(), potions.end());
+        vector<int> result;
+        for (int i = 0; i < spells.size(); i++) {
+            int left = 0, right = potions.size() - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                long long product = spells[i] * potions[mid];
+                if (product < success) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            result.push_back(potions.size() - left); 
+        }
+        return result;
+    }
+};
