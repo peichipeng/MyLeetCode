ae2479fdbb2a105645d02d68b971a593fb62a32e
peichipeng <118533573+peichipeng@users.noreply.github.com>
2025-01-23 02:31:46 +0800

    Time: 0 ms (100%), Space: 8.7 MB (43.95%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        while (true) {
+            int sum = 0;
+            while (num) {
+                sum += num % 10;
+                num /= 10;
+            }
+            if (sum < 10) return sum;
+            num = sum;
+        }
+        return -1;
+    }
+};
