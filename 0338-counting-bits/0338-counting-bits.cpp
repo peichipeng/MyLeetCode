538b62a719abd91950a161478defc2c0a77503ba
peichipeng <118533573+peichipeng@users.noreply.github.com>
2025-01-27 00:52:35 +0800

    Time: 0 ms (100%), Space: 10.5 MB (79.96%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,19 +1,10 @@
 class Solution {
-private:
-    int popCount(int x) {
-        int count = 0;
-        while (x != 0) {
-            x &= (x - 1);
-            count++;
-        }
-        return count;
-    }
 public:
     vector<int> countBits(int n) {
-        vector<int> ans(n + 1);
-        for (int i = 0; i <= n; i++) {
-            ans[i] = popCount(i);
-        }
-        return ans;
+       vector<int> ans(n + 1, 0);
+       for (int i = 1; i <= n; i++) {
+        ans[i] = ans[i >> 1] + (i & 1);
+       }
+       return ans;
     }
 };
