7b9be1b32de9396d4557f2997d033e4c83fcdc23
peichipeng <118533573+peichipeng@users.noreply.github.com>
2025-01-28 21:24:14 +0800

    Time: 0 ms (100%), Space: 65 MB (89.9%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
+    int store(vector<int>& nums, int cur) {
+        if (nums[cur] == cur) return cur;
+        int next = nums[cur];
+        nums[cur] = cur;
+        return store(nums, next);
+    }
     int findDuplicate(vector<int>& nums) {
-        int duplicate = -1;
-        for (int i = 0; i < nums.size(); i++) {
-            int cur = abs(nums[i]);
-            if (nums[cur] < 0) {
-                duplicate = cur;
-                break;
-            }
-            nums[cur] *= -1;
-        }
-        // Restore numbers
-        for (auto& num : nums) {
-            num = abs(num);
-        }
-        return duplicate;
+        return store(nums, 0);
     }
 };
