4a47856dc69af69015e473ef4df39af66ac69320
peichipeng <118533573+peichipeng@users.noreply.github.com>
2025-02-04 13:10:33 +0800

    Time: 0 ms (100%), Space: 11.2 MB (47.21%) - LeetHub

---
+++ 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int result = nums[0];
+        int sum = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > nums[i - 1]) {
+                sum += nums[i];
+            } else {
+                sum = nums[i];
+            }
+            result = max(sum, result);
+        }
+        return result;
+    }
+};
