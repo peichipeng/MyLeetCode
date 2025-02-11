8f2b81bf1eddd13b1a8e2151164fed7ed30cc546
peichipeng <118533573+peichipeng@users.noreply.github.com>
2025-02-11 18:29:11 +0800

    Time: 0 ms (100%), Space: 9.9 MB (5.27%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -4,9 +4,7 @@ public:
         int count = 0;
         unordered_map<int, int> freq;
         for (int i = 0; i < nums.size(); i++) {
-            if (freq.find(nums[i]) != freq.end()) {
-                count += freq[nums[i]];
-            }
+            count += freq[nums[i]];
             freq[nums[i]]++;
         }
         return count;
